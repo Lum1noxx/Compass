e238e09db1023e6c9e3137a5c2103bf40f75da29
Lum1noxx <wbq2004@gmail.com>
2026-05-31 02:16:48 +0800

    troubleshoot5

--- compass/wsgi.py
+++ compass/wsgi.py
@@ -10,8 +10,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compass.settings')
+
+try:
+    print("Checking and running database migrations...")
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration error occurred: {e}")
 
 application = get_wsgi_application()
 
