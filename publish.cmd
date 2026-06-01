a9ceef5d06f19e25da8f520aa8ea785fffbecf54
fireinyu <fireinyu@gmail.com>
2026-06-01 09:16:22 +0800

    fix publish zip not including subdirs

--- publish.cmd
+++ publish.cmd
@@ -2,6 +2,6 @@ cd clientapp
 call flutter pub get
 call flutter build apk
 call flutter build windows
-move build\windows\x64\runner\Release\* ..\download\windows
-move build\app\outputs\flutter-apk\app-release.apk ..\download\android\compass.apk
+robocopy build\windows\x64\runner\Release ..\download\windows /E
+copy build\app\outputs\flutter-apk\app-release.apk ..\download\android\compass.apk
 powershell -Command "Compress-Archive -Path '..\download\windows\*' -DestinationPath '..\download\windows\compass.zip' -Force"
