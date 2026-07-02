b1f47a7feb1cc264583824a7ee45b58e2c1705cc
fireinyu <fireinyu@gmail.com>
2026-07-02 14:47:22 +0800

    add website build

--- publish.cmd
+++ publish.cmd
@@ -2,6 +2,8 @@ cd clientapp
 call flutter pub get
 call flutter build apk
 call flutter build windows
-robocopy build\windows\x64\runner\Release ..\download\windows /E
+call flutter build web
 copy build\app\outputs\flutter-apk\app-release.apk ..\download\android\compass.apk
+robocopy build\web ..\website /E
+robocopy build\windows\x64\runner\Release ..\download\windows /E
 powershell -Command "Compress-Archive -Path '..\download\windows\*' -DestinationPath '..\download\windows\compass.zip' -Force"
